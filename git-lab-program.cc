9a12e9a527c78476952969dc67ec839fdcd9237d
Peyton Powers <pp805221@ohio.edu>
2022-09-09 18:47:09 -0400

    pp805221@ohio.edu I am pushing my git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,22 @@
+
+/*
+*
+ *        File: git-lab-program.cc
+ *      Author: Peyton Powers
+ *        Date: 9/9/22
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
